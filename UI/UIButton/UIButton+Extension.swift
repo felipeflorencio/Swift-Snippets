b2a349726fd9152cf61b01f486a361283c27bc12
Felipe F Garcia <felipeflorencio@me.com>
2018-07-30 09:45:04 +0200

    Helper function to you be able to rotate a button according to the device orientation

--- UI/UIButton/UIButton+Extension.swift
+++ UI/UIButton/UIButton+Extension.swift
@@ -28,4 +28,52 @@ extension UIButton {
         titleEdgeInsets = UIEdgeInsets(top: 0, left: (insetAmount * factor), bottom: 0, right: -(insetAmount * factor))
         contentEdgeInsets = UIEdgeInsets(top: 0, left: insetAmount, bottom: 0, right: insetAmount)
     }
+    
+    /* It's a helper function when you need to rotate a button, for example
+       in a camera button tha you would like to rotate the button according
+       to the device orientation.
+       Create the button as usual, can be at storyboard or programmatically,
+       and before show the button you just need to call the function:
+       Example:
+       let button = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 40))
+       button.applyButtonRotation()
+     */
+    public func applyButtonRotation() {
+		let identity = CATransform3DIdentity
+
+		func rotation() -> CATransform3D {
+
+			switch UIDevice.current.orientation {
+			case .landscapeLeft:
+				return identity.rotate(by: CGFloat.pi / 2)
+			case .landscapeRight:
+				return identity.rotate(by: -CGFloat.pi / 2)
+			case .portrait:
+				return identity
+			default:
+				return layer.transform
+			}
+		}
+
+		if isSelected || isHighlighted {
+			UIView.animate(withDuration: 0.2) {
+				self.layer.transform = rotation().scaled(by: 1.2)
+			}
+		} else {
+			UIView.animate(withDuration: 0.2) {
+				self.layer.transform = rotation()
+			}
+		}
+	}
+}
+
+// Helper to the rotation button
+extension CATransform3D {
+	func rotate(by angle: CGFloat) -> CATransform3D {
+		return CATransform3DRotate(self, angle, 0, 0, 1)
+	}
+
+	func scaled(by scale: CGFloat) -> CATransform3D {
+		return CATransform3DScale(self, scale, scale, 1)
+	}
 }
