34fc376a1368fa76c05e05f6cf8f3d6c69e3a572
Felipe F Garcia <felipeflorencio@me.com>
2018-08-13 13:05:46 +0200

    Adding helper function to add border to specific side

--- UI/UIView/UIView+Extension.swift
+++ UI/UIView/UIView+Extension.swift
@@ -40,4 +40,37 @@ public extension UIView {
             self.clipsToBounds = true
         }
     }
+    
+    // Add border to a specific position
+    enum viewBorder: String {
+        case Left = "borderLeft"
+        case Right = "borderRight"
+        case Top = "borderTop"
+        case Bottom = "borderBottom"
+    }
+    
+    // With this function you will be able to add border to a specific position or all sides
+    // any object that inherit from UIView can be used, just call this as a function on your
+    // UIView type object setting the parameters that you want
+    // For example:
+    // 'you view object reference'.addBorder(view: [.Bottom])
+    // As you can see we have some default values so onyl set if really need
+    func addBorder(view Borders: [viewBorder], color: UIColor = UIColor.lightGray, width: CGFloat = 1.0) {
+        vBorders.forEach { vBorder in
+            let border = CALayer()
+            border.backgroundColor = color.cgColor
+            border.name = vBorder.rawValue
+            switch vBorder {
+            case .Left:
+                border.frame = CGRect(x: 0, y: 0, width: width, height: self.frame.size.height)
+            case .Right:
+                border.frame = CGRect(x:self.frame.size.width - width, y: 0, width: width, height: self.frame.size.height)
+            case .Top:
+                border.frame = CGRect(x: 0, y: 0, width: self.frame.size.width, height: width)
+            case .Bottom:
+                border.frame = CGRect(x: 0, y: self.frame.size.height - width , width: self.frame.size.width, height: width)
+            }
+            self.layer.addSublayer(border)
+        }
+    }
 }
