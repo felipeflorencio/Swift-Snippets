c60e1fec246a49233d081a23025306f8b708ac83
Felipe F Garcia <felipeflorencio@me.com>
2018-09-24 17:33:49 +0200

    Adding the method to be able to get the UIColor from HEX string color

--- UI/Helpers/UIColor+Helper.swift
+++ UI/Helpers/UIColor+Helper.swift
@@ -9,7 +9,30 @@
 import UIKit
 
 extension UIColor {
-
+    /**
+        Convenience method in order to get the UIColor using a HEX color
+        How to use:
+        let color = UIColor(hexString: "#3f3f3f")
+        or if have different alpha 
+        let color = UIColor(hexString: "#3f3f3f", alpha: 0.8)
+    **/
+    convenience init(hexString: String, alpha: CGFloat = 1.0) {
+        let hexString: String = hexString.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        let scanner = Scanner(string: hexString)
+        if (hexString.hasPrefix("#")) {
+            scanner.scanLocation = 1
+        }
+        var color: UInt32 = 0
+        scanner.scanHexInt32(&color)
+        let mask = 0x000000FF
+        let r = Int(color >> 16) & mask
+        let g = Int(color >> 8) & mask
+        let b = Int(color) & mask
+        let red   = CGFloat(r) / 255.0
+        let green = CGFloat(g) / 255.0
+        let blue  = CGFloat(b) / 255.0
+        self.init(red:red, green:green, blue:blue, alpha:alpha)
+    }
     /**
      Hex string of a UIColor instance.
 
