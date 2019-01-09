802057770a1a8bd89b0d67a5631d1c0341bf6d6e
Felipe F Garcia <felipeflorencio@me.com>
2019-01-09 15:16:15 +0100

    Adding the shake extension to UIView
    Added a helper method if you want to add the shake functionality to any view, you have two parameters, one for time duration and repeat, both has default value

--- UI/UIView/UIView+Extension.swift
+++ UI/UIView/UIView+Extension.swift
@@ -73,4 +73,14 @@ public extension UIView {
             self.layer.addSublayer(border)
         }
     }
+    
+    func shake(duration timeDuration: Double = 0.07, repeat countRepeat: Float = 3){
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.duration = timeDuration
+        animation.repeatCount = countRepeat
+        animation.autoreverses = true
+        animation.fromValue = NSValue(cgPoint: CGPoint(x: self.center.x - 10, y: self.center.y))
+        animation.toValue = NSValue(cgPoint: CGPoint(x: self.center.x + 10, y: self.center.y))
+        self.layer.add(animation, forKey: "position")
+    }
 }
