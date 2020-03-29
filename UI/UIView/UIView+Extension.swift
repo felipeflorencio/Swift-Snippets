8f1b9d23189bf44f7c94ffbc47e15a26ca73c0f3
Felipe F Garcia <felipeflorencio@me.com>
2020-03-29 14:06:28 +0200

    Helper that you can easily add loader to your UIVIew

--- UI/UIView/UIView+Extension.swift
+++ UI/UIView/UIView+Extension.swift
@@ -95,4 +95,28 @@ public extension UIView {
         mask.path = path.cgPath
         layer.mask = mask
     }
+    
+    // Add Loader to your view
+    static let loadingViewTag = 1938123987
+
+    func showLoading(style: UIActivityIndicatorView.Style = .gray) {
+        var loading = viewWithTag(UIView.loadingViewTag) as? UIActivityIndicatorView
+        if loading == nil {
+            loading = UIActivityIndicatorView(style: style)
+        }
+
+        loading?.translatesAutoresizingMaskIntoConstraints = false
+        loading!.startAnimating()
+        loading!.hidesWhenStopped = true
+        loading?.tag = UIView.loadingViewTag
+        addSubview(loading!)
+        loading?.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        loading?.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+    }
+    
+    func stopLoading() {
+        let loading = viewWithTag(UIView.loadingViewTag) as? UIActivityIndicatorView
+        loading?.stopAnimating()
+        loading?.removeFromSuperview()
+    }
 }
